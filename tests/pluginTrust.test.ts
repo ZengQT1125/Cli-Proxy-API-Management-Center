ba8466ce0fa7701416a46c5d8b3fbcd72f7419fd
caidaoli <caidaoli@qq.com>
2026-07-12 21:59:09 +0800

    fix(plugins): parse official repository URLs

--- tests/pluginTrust.test.ts
+++ tests/pluginTrust.test.ts
@@ -13,4 +13,18 @@ describe('plugin store trust', () => {
     );
     expect(isOfficialPlugin(pluginEntry('official', 'someone-else/example-plugin'))).toBe(false);
   });
+
+  test('rejects encoded paths that can escape the official organization', () => {
+    const repositories = [
+      'https://github.com/router-for-me/%2e%2e/evil/repo',
+      'https://github.com/router-for-me/%2e%2e%2fevil',
+      'https://github.com/router-for-me/%2E%2E%2Fevil',
+      'https://github.com/router-for-me/%2f..%2fevil',
+      'https://github.com/router-for-me/%5c..%5cevil',
+    ];
+
+    repositories.forEach((repository) => {
+      expect(isOfficialPlugin(pluginEntry('official', repository))).toBe(false);
+    });
+  });
 });
